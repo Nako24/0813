0f81d2d16d58e2f303d4b36152535be87d3f42a2
nako24 <nhoshino@dl-techx-xedge.net>
2025-08-13 15:35:23 +0900

    ログイン・サインアップ機能追加

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ const app = new Vue({
     dialog: false,       
     dialogMessage: '' 
   },
+  methods: {
+    // DBにデータを追加する関数
+    addData: async function() {
+
+      //IDの入力チェック（空白か数字以外なら終了）
+      if(!this.ID || isNaN(this.ID)){
+        console.log("IDに数値が入力されていません");
+        return;
+      }
+      
+      //POSTメソッドで送るパラメーターを作成
+      const param = {
+        ID : this.ID,
+        Name : this.Name
+      };
+      
+      //INSERT用のAPIを呼び出し
+      const response = await axios.post('https://m3h-nako-0730.azurewebsites.net/api/INSERT?',param);
+      
+      //結果をコンソールに出力
+      console.log(response.data);
+
+    },
+    // データベースからデータを取得する関数
+    readData: async function() {
+      //SELECT用のAPIを呼び出し      
+      const response = await axios.get('https://m3h-nako-0730.azurewebsites.net/api/SELECT?');
+      
+      //結果をコンソールに出力
+      console.log(response.data);
+      
+      //結果リストを表示用配列に代入
+      this.dataList = response.data.List;
+    },
+  },
+});
